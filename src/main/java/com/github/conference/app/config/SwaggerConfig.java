e88fc56d61b0ad955c3b448d1d226053f1c2980c
Ankur Goel <ankur.goel@walmart.com>
2020-11-24 15:52:40 +0530

    adding home controller and enabling swagger

---
+++ src/main/java/com/github/conference/app/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.github.conference.app.config;
+
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2).select()
+        .apis(RequestHandlerSelectors.basePackage("com.github.conference.app"))
+        .paths(PathSelectors.any()).build().apiInfo(apiInfo());
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfo("Conference Store API Documentation", "Rest Api's for conference store",
+        "1.0", "Free to use",
+        new Contact("thecodestation", "https://github.com/thecodestation/conference-store-server",
+            "ankurgoel.juit@gmail.com"),
+        "MIT Lisence",
+        "https://github.com/thecodestation/conference-store-server/blob/master/LICENSE",
+        Collections.emptyList());
+  }
+}
---
+++ src/main/java/com/github/conference/app/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package com.github.conference.app.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+  @Value("${app.version}")
+  private String appVersion;
+
+  @GetMapping("/")
+  public Map<String, String> getStatus() {
+    final Map<String, String> map = new HashMap<>();
+    map.put("app-version", appVersion);
+    return map;
+  }
+
+}
